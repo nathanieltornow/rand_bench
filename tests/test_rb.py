b0972fee23ffe566e769138077ca04c87353d797
Nathaniel Tornow <nathaniel.tornow@tum.de>
2023-12-14 15:13:37 +0100

    clean

--- tests/test_rb.py
+++
@@ -1,9 +0,0 @@
-from rb.rand_bench import _generate_rb_circuits
-from rb.runner import SimulatorRunner
-
-
-def test_clifford_generation():
-    circuits = _generate_rb_circuits(1, 40, 1)
-    runner = SimulatorRunner()
-    prob_dists = runner.run(circuits)
-    assert all(probs["0"] == 1.0 for probs in prob_dists)
